355934f8083cfcde2600d65564ed8fee8cecacb5
L1nkr3rrr <l1nk3rino@gmail.com>
2023-08-31 13:36:13 +0300

    [CRMI-0001][DEV] - developers_management: added new base module for managing developers, creating and editing their profiles

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Developers Management',
+    'version': '16.0.0.0.0',
+    'summary': 'Module for Managing Developers',
+    'description': '''
+The "Developers Management" module is designed to streamline the process of managing developers within 
+the Odoo 16 Community platform. 
+With this module, you can easily organize and keep track of your team of developers, 
+each with their specific information and roles. 
+The module offers an intuitive interface for adding, editing, and viewing developer profiles, 
+enhancing your ability to manage and collaborate effectively.
+     ''',
+
+    'author': 'Valentyn Starushok',
+    'website': 'https://github.com/L1nk3rrr',
+    'license': 'GPL-3',
+
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/developer.xml',
+        'views/menu.xml',
+    ],
+}
---
+++ models/developer.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models 
+
+
+class Developer(models.Model):
+    _name = 'developers.management.developer'
+    _description = 'Developer management model'
+
+    name = fields.Char(
+        string='Name',
+        required=True,
+    )
+    type = fields.Selection(
+        string='Type',
+        selection=[
+            ('front-end', 'Front-end'),
+            ('backend', 'Backend'),
+            ('fullstack', 'Fullstack'),
+            ('out-stuff', 'Out-stuff')
+        ],
+        required=True,
+    )
+    global_identification = fields.Char(
+        string='Global Identification',
+        compute='_compute_global_identification',
+        store=True,
+    )
+    phone = fields.Char(string='Phone')
+    email = fields.Char(string='Email')
+    address = fields.Text(string='Address')
+    birthdate = fields.Date(string='Birthdate')
+
+    _sql_constraints = [
+        ('uniq_developer_name', 'unique(name)', 'Name of Developer must be unique')
+    ]
+
+    @api.depends('name', 'type')
+    def _compute_global_identification(self):
+        for developer in self:
+            developer.global_identification = f"{developer.name or ''}-{developer.type or ''}"
